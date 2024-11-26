a2c2affac455626337b373469005e9df7529d6e3
njiedoescs <78811505+Hehcate@users.noreply.github.com>
2024-11-25 21:35:45 -0600

    Added television and main

--- television.py
+++ television.py
@@ -7,26 +7,28 @@ class Television:
     MAX_VOLUME = 2
     MIN_CHANNEL: int = 0
     MAX_CHANNEL: int = 3
-    '''
-    Constant class variables used as limitations in methods
-    '''
     def __init__(self):
+        '''
+        Method to set the default values of the television object
+        '''
         self.__status: bool = False
         self.__muted: bool = False
         self.__volume: int = Television.MIN_VOLUME
         self.__channel: int = Television.MIN_CHANNEL
         self.__prevVolume: int = self.__volume
+    def power(self) -> None:
         '''
-        Method to set the default values of the television object
-        :param status: Televisions power status
-        :param muted: Televisions volume muting
-        :param volume: Televisions current volume
-        :param channel: Televisions current channel
+        Changes truth value of status
         '''
-    def power(self):
         self.__status = not self.__status
         # Method to switch the power of the TV
-    def mute(self):
+
+    def mute(self) -> None:
+        '''
+        Method that checks to see if the TV is already muted.
+        If not, store the volume that it was before setting the volume to 0
+        If it is, restore the volume to its previous value before being muted
+        '''
         if self.__status:
             if not self.__muted:
                 self.__previous_volume = self.__volume
@@ -35,32 +37,30 @@ class Television:
             else:  # Unmuting the TV
                 self.__volume = self.__previous_volume
                 self.__muted = False
-            '''
-            Method that checks to see if the TV is already muted.
-            If not, store the volume that it was before setting the volume to 0
-            If it is, restore the volume to its previous value before being muted
-            '''
-    def channel_up(self):
+    def channel_up(self) -> None:
+        '''
+        Method that increases the channel if it is below the MAX_CHANNEL variable
+        '''
         if self.__status == False:
             return
         if self.__channel == self.MAX_CHANNEL:
             self.__channel = self.MIN_CHANNEL
         else:
             self.__channel += 1
+    def channel_down(self) -> None:
         '''
-        Method that increases the channel if it is below the MAX_CHANNEL variable
+        Method that decreases the channel if it is above the MIN_CHANNEL variable
         '''
-    def channel_down(self):
         if self.__status == False:
             return
         if self.__channel == self.MIN_CHANNEL:
             self.__channel = self.MAX_CHANNEL
         else:
             self.__channel -= 1
+    def volume_up(self) -> None:
         '''
-        Method that decreases the channel if it is above the MIN_CHANNEL variable
+        Method that increases the volume if it is below the MAX_VOLUME variable
         '''
-    def volume_up(self):
         if not self.__status:
             return
         if self.__muted:
@@ -68,11 +68,10 @@ class Television:
             self.__volume = self.__previous_volume
         if self.__volume < self.MAX_VOLUME:
             self.__volume += 1
-
-    '''
-    Method that increases the volume if it is below the MAX_VOLUME variable
-    '''
-    def volume_down(self):
+    def volume_down(self) -> None:
+        '''
+        Method that decreases the volume if it is above the MIN_VOLUME variable
+        '''
         if not self.__status:
             return
         if self.__muted:
@@ -80,11 +79,8 @@ class Television:
             self.__volume = self.__previous_volume
         if self.__volume > self.MIN_VOLUME:
             self.__volume -= 1
-    '''
-    Method that decreases the volume if it is above the MIN_VOLUME variable
-    '''
     def __str__(self):
+        '''
+        Method that prints the Power, Channel, and Volume status when called
+        '''
         return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}'
-    '''
-    Method that prints the Power, Channel, and Volume status when called
-    '''
